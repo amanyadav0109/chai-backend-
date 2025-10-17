37124d93ed2fa08958adbd3d6ae1ac6574fe6463
amanyadav0109 <amanyadav755081@gmail.com>
2025-10-17 09:44:03 +0530

    add secure routes for user account management and profile updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 
-import {loginUser, logoutUser, registerUser,refreshAccessToken} from '../controllers/user.controller.js'
+import {loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getcurrentuser, getwatchHistory, updateAccountDetail, updateuserAvatar, updateusercoverImage, getuserChannelProfile} from '../controllers/user.controller.js'
 import { Router } from 'express';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -21,4 +21,11 @@ router.route("/login").post(loginUser)
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").post(verifyJWT,getcurrentuser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetail)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateuserAvatar)
+router.route("/cover-Image").patch(verifyJWT,upload.single("/coverImage"),updateusercoverImage)
+router.route("/c/:username").get(verifyJWT,getuserChannelProfile)
+router.route("/watch-history").get(verifyJWT,getwatchHistory)
 export default router;
